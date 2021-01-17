1f3503fa71c29205de5bc5a5215df2b6d1a3d7b9
Baggio25 <rodrigo.baggio.si@gmail.com>
2021-01-17 15:27:28 -0300

    Aula 16.7

--- src/main/java/com/algaworks/brewer/model/Cidade.java
+++ src/main/java/com/algaworks/brewer/model/Cidade.java
@@ -3,6 +3,7 @@ package com.algaworks.brewer.model;
 import java.io.Serializable;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -10,6 +11,8 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name = "cidade")
@@ -25,8 +28,9 @@ public class Cidade implements Serializable {
 	private String nome;
 
 	@NotNull
-	@ManyToOne
+	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "codigo_estado")
+	@JsonIgnore
 	private Estado estado;
 
 	public Long getCodigo() {
--- src/main/java/com/algaworks/brewer/repository/Cidades.java
+++ src/main/java/com/algaworks/brewer/repository/Cidades.java
@@ -1,5 +1,7 @@
 package com.algaworks.brewer.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,6 @@ import com.algaworks.brewer.model.Cidade;
 @Repository
 public interface Cidades extends JpaRepository<Cidade, Long> {
 
+	public List<Cidade> findByEstadoCodigo(Long codigoEstado);
+	
 }
