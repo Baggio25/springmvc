237202172ebc6fcc0e0d03aa948cf5953615270a
Baggio25 <rodrigo.baggio.si@gmail.com>
2021-01-20 22:31:17 -0300

    Aula 18.2

---
+++ src/main/java/com/algaworks/brewer/controller/DashboardController.java
@@ -0,0 +1,18 @@
+package com.algaworks.brewer.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/dashboard")
+public class DashboardController {
+
+	@GetMapping
+	public ModelAndView iniciar() {
+		ModelAndView mv = new ModelAndView("/Dashboard");
+		return mv;
+	}
+	
+}
